5380110e64e9fcccdebc2d8af33b4a7bb626945d
ervinjahic65 <ervinjahic65@gmail.com>
2025-03-20 11:10:15 +0100

    initial commit

---
+++ Harness Richer Consumer Insights at Scale – Medallia – Medallia_files/demandbase.js
@@ -0,0 +1,14 @@
+(function (d, b, a, s, e) {
+  var t = b.createElement(a),
+    fs = b.getElementsByTagName(a)[0];
+  t.async = 1;
+  t.id = e;
+  t.src = s;
+  fs.parentNode.insertBefore(t, fs);
+})(
+  window,
+  document,
+  "script",
+  "https://tag.demandbase.com/a508e5fd1748fb8e.min.js",
+  "demandbase_js_lib"
+);
